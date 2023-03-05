586bffe191dc9c40f1ec5587d1a9e81f991e3032
mixihuyen <lehuyen23vn@gmail.com>
2023-03-05 17:15:52 +0700

    fg

--- migrations/Version20230305100516.php
+++ migrations/Version20230305100516.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230304172349 extends AbstractMigration
+final class Version20230305100516 extends AbstractMigration
 {
     public function getDescription(): string
     {
